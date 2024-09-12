e936ed3cd36527d215b21d3abbfe86550f46e4de
Sonnguyenmin <nguyensoncl184@gmail.com>
2024-09-12 07:50:30 +0700

    session01

--- BaiTap/src/App.jsx
+++ BaiTap/src/App.jsx
@@ -1,32 +1,26 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import ListCourse from './components/ListCourse'
-import Calculator from './components/Calculator'
-import Information from './components/Information'
-import ColorBox from './components/ColorBox'
-import FormatName from './components/FormatName'
-import ShoppingCart from './components/shoppingCart/ShoppingCart'
-
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ListCourse from './components/ListCourse';
+import Calculator from './components/Calculator';
+import Information from './components/Information';
+import ColorBox from './components/ColorBox';
+import FormatName from './components/FormatName';
+import Ex8 from './components/Ex8';
 
 export default function App() {
-  const styled = {color: "red", fontSize: 20}
+  const styled = { color: 'red', fontSize: 20 };
   return (
     <div>
-    
-      <Header>
-      </Header>
+      <Header></Header>
       <h1 style={styled}>Component App</h1>
       <ListCourse></ListCourse>
       <Calculator></Calculator>
       <Information></Information>
       <ColorBox></ColorBox>
       <FormatName></FormatName>
-      <ShoppingCart/>
-      <Footer>
-      </Footer>
+      <Ex8 />
+      <Footer></Footer>
     </div>
-    
-  )
+  );
 }
-
